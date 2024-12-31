3350a7ae5434722e5f1b01d458c3feea72d59fea
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2024-12-30 23:54:34 -0300

    Working with application services on business layer

---
+++ src/DevIO.Business/Core/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using DevIO.Business.Core.Models;
+using FluentValidation;
+
+namespace DevIO.Business.Core.Services
+{
+    public abstract class BaseService
+    {
+        protected bool ExecutarValidacao<TV, TE>(TV validation, TE entity) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validation.Validate(entity);
+
+            return validator.IsValid;
+        }
+    }
+}
